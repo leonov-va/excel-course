7defd2289438596f2e37656f059d50a9c35f3654
Vladislav Leonov <leonov.xi@gmail.com>
2020-06-21 23:11:41 +0300

    finish base table logic

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,14 +1,33 @@
-import { ExcelComponent } from "@core/ExcelComponent";
-import { createTable } from './table.template';
-import { resizeHandler } from './table.resize';
-import { shouldResize } from './table.functions';
+import {
+  ExcelComponent
+} from "@core/ExcelComponent";
+import {
+  $
+} from '@core/dom.js';
+import {
+  createTable
+} from './table.template';
+import {
+  resizeHandler
+} from './table.resize';
+import {
+  shouldResize,
+  isCell,
+  matrix,
+  nextSelector
+} from './table.functions';
+import {
+  TableSelection
+} from './TableSelection';
 
 export class Table extends ExcelComponent {
   static className = 'excel__table';
-  
-  constructor($root) {
+
+  constructor($root, options) {
     super($root, {
-      listeners: ['click', 'mousedown', 'mousemove', 'mouseup']
+      name: 'Table',
+      listeners: ['mousedown', 'keydown', 'input'],
+      ...options
     })
   }
 
@@ -16,30 +35,60 @@ export class Table extends ExcelComponent {
     return createTable();
   }
 
-  onClick() {
-    // console.log('click');
+  prepare() {
+    console.log('prepare');
+    this.selection = new TableSelection();
+  }
+
+  init() {
+    console.log('init');
+    super.init();
+
+    const $cell = this.$root.find('[data-id="0:0"]');
+    this.selectCell($cell);
+
+    this.$on('formula:input', text => {
+      this.selection.current.text(text);
+    });
+
+    this.$on('formula:done', () => {
+      this.selection.current.focus();
+    })
+  }
+
+  selectCell($cell) {
+    this.selection.select($cell);
+    this.$emit('table:select', $cell);
   }
 
   onMousedown(event) {
     if (shouldResize(event)) {
       resizeHandler(this.$root, event);
+    } else if (isCell(event)) {
+      const $target = $(event.target);
+      if (event.shiftKey) {
+        const $cells = matrix($target, this.selection.current)
+          .map(id => this.$root.find(`[data-id="${id}"]`));
+        this.selection.selectGroup($cells);
+      } else {
+        this.selection.select($target);
+      }
     }
   }
 
-  onMousemove() {
-    // console.log('mousemove');
+  onKeydown(event) {
+    const keys = ['Enter', 'Tab', 'ArrowLeft', 'ArrowRight', 'ArrowDown', 'ArrowUp'];
+    const {key} = event;
+    
+    if (keys.includes(key) && !event.shiftKey) {
+      event.preventDefault();
+      const id = this.selection.current.id(true);
+      const $next = this.$root.find(nextSelector(key, id));
+      this.selectCell($next);
+    }
   }
 
-  onMouseup() {
-    // console.log('mouseup');
+  onInput(event) {
+    this.$emit('table:input', $(event.target));
   }
 }
-
-// 470 msScripting
-// 2977 msRendering
-
-// 143 msScripting
-// 2460 msRendering
-
-// 297 msScripting
-// 573 msRendering
